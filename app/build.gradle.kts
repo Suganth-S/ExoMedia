ddc233e93989f8c363f927dfd80fa600c4f15123
Suganth <suganth.sakthivel@ideas2it.com>
2026-01-05 23:58:39 +0530

    Exoplayer dependency added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,9 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    implementation(libs.androidx.media3.exoplayer)
+    implementation(libs.androidx.media3.dash)
+    implementation(libs.androidx.media3.ui)
+    implementation(libs.androidx.media3.ui.compose)
 }
