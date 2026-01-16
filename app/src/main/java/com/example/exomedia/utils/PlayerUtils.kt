a263f25fff6206f9145d145524d29a46dbf2e3a2
Suganth S <suganth.sakthivel@ideas2it.com>
2026-01-16 23:10:08 +0530

    Setting up viewmodel

---
+++ app/src/main/java/com/example/exomedia/utils/PlayerUtils.kt
@@ -0,0 +1,8 @@
+package com.example.exomedia.utils
+
+object PlayerUtils {
+    const val Video_1 = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+    const val Video_2 = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4"
+    const val Video_3 = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"
+    const val Video_4 = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4"
+}
