4398c758a7b2a2f725a41f57c07ba6a1496e955a
Suganth S <suganth.sakthivel@ideas2it.com>
2026-01-17 20:31:58 +0530

    Player Screen and Video list updated

--- app/src/main/java/com/example/exomedia/ui/screens/PlayerListScreen.kt
+++ app/src/main/java/com/example/exomedia/ui/screens/PlayerListScreen.kt
@@ -6,11 +6,24 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.compose.PlayerSurface
 
 @Composable
-fun PlayerListScreen(modifier: Modifier = Modifier, exoPlayer: ExoPlayer) {
+fun PlayerRoute(modifier: Modifier = Modifier, playerViewModel: PlayerViewModel = viewModel()) {
+    val exoPlayer = playerViewModel.playerState.collectAsStateWithLifecycle()
+
+    Box(modifier.fillMaxWidth()) {
+        exoPlayer.value?.let {
+            PlayerScreen(exoPlayer = it)
+        }
+    }
+}
+
+@Composable
+fun PlayerScreen(modifier: Modifier = Modifier, exoPlayer: ExoPlayer) {
     Box(modifier = modifier) {
         PlayerSurface(
             player = exoPlayer,
--- app/src/main/java/com/example/exomedia/utils/PlayerUtils.kt
+++ app/src/main/java/com/example/exomedia/utils/PlayerUtils.kt
@@ -5,4 +5,13 @@ object PlayerUtils {
     const val Video_2 = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4"
     const val Video_3 = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"
     const val Video_4 = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4"
+    const val Video_5 = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4"
+    const val Video_6 = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4"
+    const val Video_7 = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4"
+    const val Video_8 = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4"
+    const val Video_9 = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4"
+    const val Video_10 = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4"
+    const val Video_11 = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/VolkswagenGTIReview.mp4"
+    const val Video_12= "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4"
+    const val Video_13= "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WhatCarCanYouGetForAGrand.mp4"
 }
