6bf97671cf3427f06ebbbd8b39d8b0d617f7a357
Suganth S <suganth.sakthivel@ideas2it.com>
2026-01-19 19:02:04 +0530

    Created Player wiht media item

--- app/src/main/java/com/example/exomedia/ui/screens/PlayerViewModel.kt
+++ app/src/main/java/com/example/exomedia/ui/screens/PlayerViewModel.kt
@@ -1,14 +1,33 @@
 package com.example.exomedia.ui.screens
 
+import android.content.Context
+import androidx.compose.runtime.Composable
 import androidx.lifecycle.ViewModel
+import androidx.media3.common.MediaItem
 import androidx.media3.exoplayer.ExoPlayer
+import com.example.exomedia.utils.PlayerUtils
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 
 @HiltViewModel
 class PlayerViewModel : ViewModel() {
 
     private val _playerState = MutableStateFlow<ExoPlayer?>(null)
     val playerState: StateFlow<ExoPlayer?> = _playerState
+
+
+    fun createPlayerWithMediaItems(context: Context) {
+        if(_playerState.value == null){
+            val mediaItem = MediaItem.Builder().setUri(PlayerUtils.Video_1).build()
+            _playerState.update {
+                ExoPlayer.Builder(context).build().apply {
+                    setMediaItem(mediaItem)
+                    prepare()
+                    playWhenReady= true
+                }
+            }
+        }
+    }
 }
