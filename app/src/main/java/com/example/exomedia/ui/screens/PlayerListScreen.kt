07df8647c1158cc0bfcf5d531e5e9ce1a5d7a30e
Suganth S <suganth.sakthivel@ideas2it.com>
2026-01-20 23:30:27 +0530

    Adding LaunchedEffect for Video Playback

--- app/src/main/java/com/example/exomedia/ui/screens/PlayerListScreen.kt
+++ app/src/main/java/com/example/exomedia/ui/screens/PlayerListScreen.kt
@@ -4,8 +4,10 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.media3.exoplayer.ExoPlayer
@@ -14,11 +16,16 @@ import androidx.media3.ui.compose.PlayerSurface
 @Composable
 fun PlayerRoute(modifier: Modifier = Modifier, playerViewModel: PlayerViewModel = viewModel()) {
     val exoPlayer = playerViewModel.playerState.collectAsStateWithLifecycle()
+    val context = LocalContext.current
 
     Box(modifier.fillMaxWidth()) {
         exoPlayer.value?.let {
             PlayerScreen(exoPlayer = it)
         }
+    }
+
+    LaunchedEffect(Unit) {
+        playerViewModel.createPlayerWithMediaItems(context)
     }
 }
 
