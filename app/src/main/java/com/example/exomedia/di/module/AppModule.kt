bb5bcf7bb12925151750eaaa96ce6f145a230103
Suganth S <suganth.sakthivel@ideas2it.com>
2026-01-24 17:29:54 +0530

    OkHttp Loging Integration

--- app/src/main/java/com/example/exomedia/di/module/AppModule.kt
+++ app/src/main/java/com/example/exomedia/di/module/AppModule.kt
@@ -1,22 +1,35 @@
 package com.example.exomedia.di.module
 
+import com.example.exomedia.data.api.NetworkService
 import com.example.exomedia.utils.AppConstant.BASE_URL
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
+    private const val TIMEOUT_SECONDS = 120L // 2 Minutes
+
     @Singleton
     @Provides
-    fun provideOkHttpClient() {
-
+    fun provideOkHttpClient() : OkHttpClient{
+        val logging = HttpLoggingInterceptor()
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+        return OkHttpClient.Builder()
+            .addInterceptor (logging)
+            .connectTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
+            .readTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
+            .writeTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
+            .build()
     }
 
     @Provides
@@ -24,9 +37,16 @@ object AppModule {
 
     @Singleton
     @Provides
-    fun provideRetrofit(): Retrofit = Retrofit.Builder()
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
+        .client(okHttpClient)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
+
+    @Singleton
+    @Provides
+    fun provideNetworkService(retrofit: Retrofit): NetworkService {
+        return retrofit.create(NetworkService::class.java)
+    }
 
 }
