0c92ae4f896d9032477673c6eea66069c82b4d8b
Suganth S <suganth.sakthivel@ideas2it.com>
2026-01-30 21:14:15 +0530

    Viewmodel api call for video list

--- app/src/main/java/com/example/exomedia/ui/screens/PlayerViewModel.kt
+++ app/src/main/java/com/example/exomedia/ui/screens/PlayerViewModel.kt
@@ -3,29 +3,36 @@ package com.example.exomedia.ui.screens
 import android.content.Context
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.media3.common.MediaItem
 import androidx.media3.exoplayer.ExoPlayer
+import com.example.exomedia.data.repository.VideoListRepository
 import com.example.exomedia.utils.PlayerUtils
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
+import javax.inject.Inject
 
 @HiltViewModel
-class PlayerViewModel : ViewModel() {
+class PlayerViewModel @Inject constructor(
+    private val repository: VideoListRepository
+) : ViewModel() {
 
     private val _playerState = MutableStateFlow<ExoPlayer?>(null)
     val playerState: StateFlow<ExoPlayer?> = _playerState
 
+    fun getVideoList(key: String, videoType: String) =
+        repository.getVideoList(key, videoType)
 
     fun createPlayerWithMediaItems(context: Context) {
-        if(_playerState.value == null){
+        if (_playerState.value == null) {
             val mediaItem = MediaItem.Builder().setUri(PlayerUtils.Video_1).build()
             _playerState.update {
                 ExoPlayer.Builder(context).build().apply {
                     setMediaItem(mediaItem)
                     prepare()
-                    playWhenReady= true
+                    playWhenReady = true
                 }
             }
         }
