36bc7d0f843305bfca0f80d3bb6c758b061310e4
Suganth S <suganth.sakthivel@ideas2it.com>
2026-02-02 23:54:49 +0530

    Video list state flow view model implementation

--- app/src/main/java/com/example/exomedia/ui/screens/PlayerViewModel.kt
+++ app/src/main/java/com/example/exomedia/ui/screens/PlayerViewModel.kt
@@ -6,24 +6,38 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.media3.common.MediaItem
 import androidx.media3.exoplayer.ExoPlayer
+import com.example.exomedia.data.model.VideoListResponse
 import com.example.exomedia.data.repository.VideoListRepository
+import com.example.exomedia.ui.base.UiState
 import com.example.exomedia.utils.PlayerUtils
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class PlayerViewModel @Inject constructor(
     private val repository: VideoListRepository
 ) : ViewModel() {
+    private val _videoListStateFlow = MutableStateFlow<UiState<VideoListResponse>>(UiState.Loading)
+    val videoListStateFlow: StateFlow<UiState<VideoListResponse>> = _videoListStateFlow
 
     private val _playerState = MutableStateFlow<ExoPlayer?>(null)
     val playerState: StateFlow<ExoPlayer?> = _playerState
 
-    fun getVideoList(key: String, videoType: String) =
-        repository.getVideoList(key, videoType)
+    fun getVideoList(key: String, videoType: String) {
+        viewModelScope.launch {
+            repository.getVideoList(key, videoType).collect { response ->
+                if (response != null) {
+                    _videoListStateFlow.value = UiState.Success(response)
+                } else {
+                    _videoListStateFlow.value = UiState.Error("Error fetching video list")
+                }
+            }
+        }
+    }
 
     fun createPlayerWithMediaItems(context: Context) {
         if (_playerState.value == null) {
